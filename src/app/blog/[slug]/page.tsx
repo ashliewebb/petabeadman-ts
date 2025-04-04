f9ff361192fbd30f2ea9fa2cc660789d03c602fa
Ashlie Webb <greetings@ashliewebb.com>
2025-04-04 14:51:18 +1100

    Comment out incomplete blog post so I can deploy

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,16 @@
-import { getBlogPostBySlug } from '@/lib/prismic/queries';
-
-export default async function BlogPostPage({ params }) {
-    const post = await getBlogPostBySlug(params.slug);
-
-    return (
-        <article>
-            <h1>{post.data.title}</h1>
-            <div>{/* Rich text rendering here */}</div>
-        </article>
-    );
-}
+// import { getBlogPostBySlug } from '@/lib/prismic/queries';
+//
+// type BlogPostPageProps = {
+//     params: { slug: string };
+// };
+//
+// export default async function BlogPostPage({ params }: BlogPostPageProps) {
+//     const post = await getBlogPostBySlug(params.slug);
+//
+//     return (
+//         <article>
+//             {/*<h1>{post.data.title}</h1>*/}
+//             {/* your content */}
+//         </article>
+//     );
+// }
