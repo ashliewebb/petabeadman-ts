b706c7e1897a3fe55c3c8fad1a295f92f9021613
Ashlie Webb <greetings@ashliewebb.com>
2025-06-26 14:23:55 +1000

    Refine styles of product cards

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -18,13 +18,13 @@ type ProductProps = {
 
 export const Product = ({title, description, image, imageAlt, imageWidth, imageHeight, price, shipping, linkUrl, linkLabel, mostPopular}: ProductProps) => {
     return (
-        <div className={`${styles.product} ${mostPopular && styles.productMostPopular}`}>
+        <div className={`${styles.product} ${mostPopular ? styles.productMostPopular : ''}`}>
             {image && (
                 <img className={styles.productImage} src={image} alt={imageAlt} width={imageWidth} height={imageHeight}/>
             )}
             <h3>{title}</h3>
-            {description}
-            {price && <p>{price}<span className={styles.productCurrency}>AUD</span></p>}
+            {description && <div className="productDesc">{description}</div>}
+            {price && <p className={styles.productPrice}>{price}<span className={styles.productCurrency}>AUD</span></p>}
             {shipping && <p>{shipping}</p>}
             <Link className={`buttonPrimary`} href={linkUrl} target={'_blank'}>{linkLabel}</Link>
         </div>
